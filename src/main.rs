ffc48053fa825bc0cd9da91e0225a9e27faff31b
Luke Briggs <22104392+LukeBriggsDev@users.noreply.github.com>
2024-03-15 17:51:51 +0000

    Add Vault struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use time;
+use argon2;
+use argon2::{Argon2, PasswordHasher};
+use argon2::password_hash::SaltString;
+use rand;
+use chrono;
+use chrono::{DateTime, Utc};
 
 struct Credential {
     service_name: String,
@@ -6,11 +11,11 @@ struct Credential {
     username: String,
     password: String,
     notes: String,
-    date_added: time::Date,
+    date_added: DateTime<Utc>,
 }
 
 impl Credential {
-    fn new(service_name: String, service_url: String, username: String, password: String, notes: String, date_added: time::Date) -> Credential {
+    fn new(service_name: String, service_url: String, username: String, password: String, notes: String, date_added: DateTime<Utc>) -> Credential {
         Credential {
             service_name,
             service_url: Some(service_url),
@@ -18,6 +23,26 @@ impl Credential {
             password,
             notes,
             date_added,
+        }
+    }
+}
+
+struct Vault {
+    credentials: Vec<Credential>,
+    salt: SaltString,
+    key: String,
+    creation_date: DateTime<Utc>,
+}
+
+impl Vault {
+    pub fn new(password: String) -> Vault {
+        let argon2 = Argon2::default();
+        let salt = SaltString::generate(&mut rand::rngs::OsRng);
+        Vault {
+            credentials: Vec::new(),
+            salt: salt,
+            key: argon2.hash_password(password.as_ref(), &salt)?.to_string(),
+            creation_date: chrono::offset::Utc::now()
         }
     }
 }
