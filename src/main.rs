aae7b9b65c823d5224911c24c48cbc99ffd04912
gwo0d <git@gwood.dev>
2024-03-15 17:16:26 +0000

    Add Credential Struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use time;
+
+struct Credential {
+    service_name: String,
+    service_url: Option<String>,
+    username: String,
+    password: String,
+    notes: String,
+    date_added: time::Date,
+}
+
+impl Credential {
+    fn new(service_name: String, service_url: String, username: String, password: String, notes: String, date_added: time::Date) -> Credential {
+        Credential {
+            service_name,
+            service_url: Some(service_url),
+            username,
+            password,
+            notes,
+            date_added,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
