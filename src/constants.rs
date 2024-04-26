55ef59c10968516e2184354a9eed6a3bfeb107ec
gwo0d <git@gwood.dev>
2024-04-26 15:34:24 +0100

    Implement generate_salt() in utils.rs - Moved constants to constants.rs

---
+++ src/constants.rs
@@ -0,0 +1,2 @@
+pub const KEY_SIZE: usize = 32;
+pub const SALT_SIZE: usize = 32;
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,15 @@
 use argon2::Argon2;
-
-const KEY_SIZE: usize = 32;
+use rand::{RngCore, rngs::OsRng};
+use crate::constants::{KEY_SIZE, SALT_SIZE};
 
 pub fn derive_key(password: &[u8], salt: &[u8]) -> [u8; KEY_SIZE] {
     let mut key = [0u8; KEY_SIZE];
     Argon2::default().hash_password_into(password, salt, &mut key).expect("\nKey Derivation Failed\n");
     key
+}
+
+pub fn generate_salt() -> [u8; SALT_SIZE] {
+    let mut salt = [0u8; SALT_SIZE];
+    OsRng.fill_bytes(&mut salt);
+    salt
 }
